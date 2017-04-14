305f006bc8df66945dcb698a47d5f1a4d67631dc
Chandler J. Wang <jwwang07@gmail.com>
2017-04-14 16:19:14 -0700

    add user model and migration and inital validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+	has_secure_password
+	validates :name, presence: true, length: { maximum: 50 }
+	validates :password, presence: true, length: { minimum: 5 }
+
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+	validates :email, presence: true, length: {maximum: 255 },
+					  uniqueness: { case_sensitive: false },
+					  format: { with: VALID_EMAIL_REGEX }
 end
