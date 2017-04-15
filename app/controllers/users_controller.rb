aabd315d234a5c897210fc9a3a89b1b4c446779f
Chandler J. Wang <jwwang07@gmail.com>
2017-04-14 19:09:01 -0700

    added the flash method as one time alert

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def index
-    
+    @users = User.all    
   end
 
   def show
@@ -14,6 +14,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      flash[:success] = "Welcome to Chat!"
       redirect_to @user
     else
       render 'users/new'  
