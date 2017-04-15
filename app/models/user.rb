37e843557b6e580e63c681437ad88e0eca926ddc
Chandler J. Wang <jwwang07@gmail.com>
2017-04-14 21:57:08 -0700

    created session controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+	before_save { self.email = email.downcase }
 	has_secure_password
 	validates :name, presence: true, length: { maximum: 50 }
 	validates :password, presence: true, length: { minimum: 5 }
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   root 'static_pages#home'
   resources :users
 
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
 end
