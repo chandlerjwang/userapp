82838466838cd410973e31c6b525b011e6e89380
Chandler J. Wang <jwwang07@gmail.com>
2017-04-14 22:01:26 -0700

    make sessionshelper available to controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
-  def new
-  	render html: "welcome - site is still being developed...check back later!"
-  end
+  include SessionsHelper
 end
