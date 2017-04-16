6c81cb8eaae85a42a9f1acbc33f283acfc3d51d4
Chandler J. Wang <jwwang07@gmail.com>
2017-04-15 23:52:44 -0700

    minor change

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
 	# Returns the full title on a per page basis
 	def full_title(page_title = '')
-		base_title = "ChatApp"
+		base_title = "UserApp"
 		page_title.empty? ? base_title : page_title + " | " + base_title
 	end
 end
