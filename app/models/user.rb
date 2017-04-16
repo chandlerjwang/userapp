60a6a650bdd53d54402cde4f368acbc3800843d1
Chandler J. Wang <jwwang07@gmail.com>
2017-04-15 17:21:31 -0700

    profile edit

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,8 @@ class User < ApplicationRecord
 	before_save { self.email = email.downcase }
 	has_secure_password
 	validates :name, presence: true, length: { maximum: 50 }
-	validates :password, presence: true, length: { minimum: 5 }
+	validates :password, presence: true, length: { minimum: 5 },
+										  allow_nil: true
 
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 	validates :email, presence: true, length: {maximum: 255 },
