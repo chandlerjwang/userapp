26b364a33869d8e0bdaf02f025f518ad39eef438
Chandler J. Wang <jwwang07@gmail.com>
2017-04-20 00:21:49 -0700

    add chatroom controller

---
+++ app/controllers/chatrooms_controller.rb
@@ -0,0 +1,45 @@
+class ChatroomsController < ApplicationController
+	before_action :require_user_login, only: [:index, :new, :create, :show]
+
+	def index
+		@chatrooms = Chatroom.all
+	end
+
+	def new
+		@chatroom = Chatroom.new
+	end
+
+	def create
+		@chatroom = Chatroom.new(chatroom_params)
+		if @chatroom.save
+			flash[:success] = "Chatroom created!"
+			redirect_to @chatroom
+		else
+			render 'new'
+		end
+	end
+
+	def show
+	end
+
+	def destroy
+		Chatroom.find_by(id: params[:id]).destroy
+		flash[:success] = "Chatroom deleted"
+		redirect_to chatrooms_path
+	end
+
+
+	private
+
+		def chatroom_params
+			params.require(:chatroom).permit(:topic)
+		end
+
+	# site authorization
+	def require_user_login
+	  unless logged_in?
+	    flash[:danger] = "Please log in"
+	    redirect_to login_path
+	  end
+	end  	
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
-  get 'password_resets/new'
-
-  get 'password_resets/edit'
-
   root 'static_pages#home'
   resources :users
+  resources :chatrooms
   resources :account_activation, only: [:edit]
   resources :password_resets, only: [:new, :create, :edit, :update]
 
